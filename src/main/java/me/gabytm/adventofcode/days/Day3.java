63ec45c5c7aa046b322c4e9855baf7b18f5b3ae0
iGabyTM <contactgabytm@gmail.com>
2021-12-04 16:31:22 +0200

    add day 3 part 2

--- src/main/java/me/gabytm/adventofcode/days/Day3.java
+++ src/main/java/me/gabytm/adventofcode/days/Day3.java
@@ -1,6 +1,8 @@
 package me.gabytm.adventofcode.days;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +20,53 @@ public class Day3 extends Day<Integer> {
         return Arrays.stream(getInput(1).split("\n"))
                 .map(String::trim)
                 .collect(Collectors.toList());
+    }
+
+    private char findCommonBit(final List<String> list, final int index, final boolean mostCommon) {
+        var ones = 0;
+        var zeros = 0;
+
+        for (final var it : list) {
+            if (it.charAt(index) == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+
+        // oxygen generator
+        if (mostCommon) {
+            if (ones == zeros) {
+                return '1';
+            }
+
+            return ones > zeros ? '1' : '0';
+        }
+
+        // CO2 scrubber
+        if (ones == zeros) {
+            return '0';
+        }
+
+        return ones < zeros ? '1' : '0';
+    }
+
+    private Integer findRating(final List<String> diagnosticReport, final boolean oxygenGenerator) {
+        final var copy = new ArrayList<>(diagnosticReport);
+        final var itemLength = copy.get(0).length();
+
+        for (int i = 0; i < itemLength; i++) {
+            final var commonBit = findCommonBit(copy, i, oxygenGenerator);
+            final var index = i;
+
+            copy.removeIf(it -> it.charAt(index) != commonBit);
+
+            if (copy.size() == 1) {
+                return Integer.parseInt(copy.get(0), 2);
+            }
+        }
+
+        throw new IllegalArgumentException("Could not find rating");
     }
 
     @Override
@@ -55,6 +104,8 @@ public class Day3 extends Day<Integer> {
 
     @Override
     public Integer solveSecondPart() {
-        return null;
+        final var report = getDiagnosticReport();
+        return findRating(report, true) * findRating(report, false);
     }
+
 }
